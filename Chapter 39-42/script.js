9ed83aa4cc3291b2ecc994f589db6b5db2ca7801
Husban-Ali <139506919+Husban-Ali@users.noreply.github.com>
2023-11-10 23:10:20 +0500

    Chapter 39-42

---
+++ Chapter 39-42/script.js
@@ -0,0 +1,78 @@
+// Chapter 39 to 42
+
+// function power(a,b){
+    
+//     return a**b;
+
+// }
+// document.write(power(3,3))
+
+// function leapyear(){
+//     var a = prompt( "Enter year");
+//     if( a%4 == 0  ){
+//         document.write(a , " The enter year is leap year")
+//     }
+//     else
+//     {
+//         document.write(a , " The enter year is not leap year")
+//     }
+// };
+//  leapyear();
+
+// function area (a,b,c){
+//  var a  ;
+//  var b  ;
+//  var c  ;
+//  var s = (a+b+c)/3;
+//  var d = s(s-a)(s-b)(s-c);
+//  return d ;
+// }
+
+// area(3,5,8);
+// function student(a,b,c)
+// {
+//     function average(a,b,c){
+//         var avg = (a+b+c)/3;
+//       document.write("The Average is:",avg, "<br>")}
+
+//      function percent(a,b,c){
+//         var per = (((a+b+c)/300)*100);
+//            document.write("The Percentage is:",per,"% <br>")}
+     
+//   percent(a,b,c);
+//   average(a,b,c);
+//     }
+
+
+// document.write(student(98,79,54))
+
+// function indexof(a){
+//  var a = "Hamiz";
+//  var b =  "i";
+// for(i=0;i<=a.length;i++){
+//     if(a[i]===b){
+//         return i;
+//     }
+// }
+// }
+// console.log(indexof())
+
+// function remove(){
+//     var word = "Hello My Name is Husban";
+    
+    
+            
+//             var word1= word.replace(/[aeiou]/g, "")
+       
+//     return word1;
+
+// }
+// console.log(remove())
+// function occurence(){
+//     var a = 'Pleases read this application and give me gratuity';
+//     // switch (a ||){
+//     //     case ""
+//     // }
+    
+
+// }
